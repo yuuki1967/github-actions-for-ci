e4652f6c8451e583c542304615e9c0d9196b250b
Yuki Endo <yuki06.endo@gmail.com>
2022-06-27 14:54:21 +0900

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -19,7 +19,7 @@ describe('Game', () => {
   describe('Game', () => {
     it('Initializes with two players', async () => {
       expect(game.p1).toBe('Salem')
-      expect(game.p2).toBe('Nate')
+      expect(game.p2).toBe('Bananas')
     })
 
     it('Initializes with an empty board', async () => {
@@ -35,7 +35,7 @@ describe('Game', () => {
       expect(new Game(p1, p2).player).toBe('Salem')
 
       Math.random = () => 0.6
-      expect(new Game(p1, p2).player).toBe('Nate')
+      expect(new Game(p1, p2).player).toBe('Bananas')
     })
   })
 
@@ -57,7 +57,7 @@ describe('Game', () => {
       const game = new Game(p1, p2)
       expect(game.player).toBe('Salem')
       game.nextPlayer()
-      expect(game.player).toBe('Nate')
+      expect(game.player).toBe('Bananas')
     })
   })
 
